7437ce320151f949a9fc5c6ca83ac311d4239223
ebhargro <ebonyhargro@Ebonys-MacBook-Pro-2.local>
2023-09-03 22:09:57 -0700

    adding array

--- helloworld/src/main/java/com/testdemo/App.java
+++ helloworld/src/main/java/com/testdemo/App.java
@@ -7,5 +7,8 @@ package com.testdemo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // testing array data structure
+    String[] test = {"hello", "this", "is", "a", "test"};
+    test[3] = "ebony";
     }
 }
